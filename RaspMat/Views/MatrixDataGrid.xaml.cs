5a4d504c070cdbbdc302f3f39179d377eb7800e4
yetanotherstrawberry <106765930+yetanotherstrawberry@users.noreply.github.com>
2026-09-05 22:46:52 +0200

    matrix as component

---
+++ RaspMat/Views/MatrixDataGrid.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace RaspMat.Views
+{
+    internal partial class MatrixDataGrid : DataGrid
+    {
+
+        /// <summary>
+        /// Initializes XAML.
+        /// </summary>
+        public MatrixDataGrid()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
